edbdfc9116f0480081a0f4b3efa83c9ceaf765d1
Waleed Nasir <waleednasir186@gmail.com>
2022-12-16 16:05:55 +0500

    initial commit

---
+++ LeetCode - First bad Version/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LeetCode___First_bad_Version
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        
+        public static int FirstBadVersion(int n) {
+
+            int startIndex = 0;
+            int endIndex = n - 1;
+            while(startIndex <= endIndex){
+
+                int midIndex = startIndex + (endIndex - startIndex) / 2;
+
+                if(IsBadVersion(midIndex)){
+                    endIndex = midIndex - 1;
+                }
+                else{
+                    startIndex = midIndex + 1;
+                }
+            }
+
+            return startIndex;
+        }
+    }
+}
